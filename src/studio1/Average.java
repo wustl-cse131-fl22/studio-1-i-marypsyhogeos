c43ae41f907ff28eee62cb18c267fa6f3cf3366d
marypsyhogeos <marypsyhogeos@localhost>
2022-09-08 12:38:13 -0500

    Average Complete

---
+++ src/studio1/Average.java
@@ -0,0 +1,20 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("What is your first integer?");
+		int n1 = in.nextInt();
+		System.out.println("What is your second integer?");
+		int n2 = in.nextInt();
+		double average = (n1+n2)/2.0;
+		System.out.println(average);
+		
+
+	}
+
+}
